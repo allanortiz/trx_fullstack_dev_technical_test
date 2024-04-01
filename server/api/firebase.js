2fb319fc92f1dcda933124cabb77009211db60dc
Allan Ortíz <7284912+allanortiz@users.noreply.github.com>
2024-04-01 06:45:53 -0600

    Vercel updates for deployment

---
+++ server/api/firebase.js
@@ -0,0 +1,44 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getDatabase,
+  ref,
+  onValue,
+  push,
+  set,
+  get,
+  query,
+  startAt,
+  endAt,
+  orderByChild,
+  equalTo,
+  orderByValue,
+  limitToFirst,
+} from 'firebase/database';
+
+const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyCjHdELmBXbvrbR_KotkojBdkPNU65LuuQ',
+  authDomain: 'trixon-675bc.firebaseapp.com',
+  databaseURL: 'https://trixon-675bc-default-rtdb.firebaseio.com',
+  projectId: 'trixon-675bc',
+  storageBucket: 'trixon-675bc.appspot.com',
+  messagingSenderId: '78730342571',
+  appId: '1:78730342571:web:78a8e5caa1d4cfcd9487ee',
+};
+
+const app = initializeApp(FIREBASE_CONFIG);
+
+export {
+  getDatabase,
+  ref,
+  onValue,
+  push,
+  set,
+  get,
+  query,
+  startAt,
+  endAt,
+  orderByChild,
+  equalTo,
+  orderByValue,
+  limitToFirst,
+};
